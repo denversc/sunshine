03c30f83dc90aabf77d9f794eb94685cee4e2422
Denver Coneybeare <denver@sleepydragon.org>
2014-08-04 17:28:38 -0400

    Add Settings activity (blank so far)

--- app/src/main/java/org/sleepydragon/sunshine/DetailActivity.java
+++ app/src/main/java/org/sleepydragon/sunshine/DetailActivity.java
@@ -1,6 +1,7 @@
 package org.sleepydragon.sunshine;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.Menu;
@@ -27,6 +28,24 @@ public class DetailActivity extends Activity {
                 .penaltyDeath()
                 .build();
         StrictMode.setThreadPolicy(threadPolicy);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                final Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
--- app/src/main/java/org/sleepydragon/sunshine/MainActivity.java
+++ app/src/main/java/org/sleepydragon/sunshine/MainActivity.java
@@ -1,6 +1,7 @@
 package org.sleepydragon.sunshine;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.Menu;
@@ -30,20 +31,20 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                final Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 
 }
